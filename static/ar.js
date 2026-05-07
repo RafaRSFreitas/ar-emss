36d42d7940245a51961a13b05e25ecd4e2b49c96
CarlosRB77 <s5713792@bournemouth.ac.uk>
2026-05-07 19:51:42 +0100

    new file:   static/ar.html new file:   static/ar.js

---
+++ static/ar.js
@@ -0,0 +1,240 @@
+let currentFaultId = null; // stores currently selected fault
+let faultPannel = null;   // stores the 3d box shown in the Tree.js scene
+let scene, camera, renderer;      //objects to display the 3d graphics
+
+
+
+async function getFault(fault_id) {
+  const response = await fetch(`/api/faults/${fault_id}`);
+
+  if (!response.ok) {
+    throw new Error("Fault not found");
+  }
+
+  return response.json();
+}
+// function getFault asks the back end for fault_id and gets the data relating to the fault and returns an error message if there is no fault
+
+
+
+async function updateFault(fault_id, status) {
+  const response = await fetch(`/api/faults/${fault_id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      status: status
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error("Could not update fault");
+  }
+
+  return response.json();
+}
+// funtion updateFault will update the fault status in the backend from the front end, so user can mark fault as fixed changing the status to closed
+
+
+
+function initThreeScene() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    100
+  );
+
+  camera.position.z = 4;
+
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const light = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(light);
+
+  const grid = new THREE.GridHelper(10, 10);
+  scene.add(grid);
+
+  animate();
+}
+// this function sets up the AR 3D scence and alters the view for the user to see the scene better with camera angle, lighting and a grid to map out the space
+
+
+
+function createFaultPanel(fault_data) {
+  if (faultPanel) {
+    scene.remove(faultPanel);
+  }
+
+  const geometry = new THREE.BoxGeometry(2.2, 1.1, 0.1);
+
+  let colour = 0x00aa00;
+
+  if (fault_data.severity === "high") {
+    colour = 0xff0000;
+  } else if (fault_data.severity === "medium") {
+    colour = 0xffaa00;
+  }
+
+  const material = new THREE.MeshBasicMaterial({
+    color: colour
+  });
+
+  faultPanel = new THREE.Mesh(geometry, material);
+  faultPanel.position.set(0, 0, 0);
+
+  scene.add(faultPanel);
+}
+// function createFaultPannel creates the pannel to see the fault "pretty self explanatory lol"... if there is already one open it will close it before opening the new one
+
+
+
+function updateFaultInfo(fault_data) {
+  document.getElementById("faultInfo").innerHTML = `
+    <strong>${fault_data.title}</strong><br>
+    Location: ${fault_data.location}<br>
+    Severity: ${fault_data.severity}<br>
+    Status: ${fault_data.status}
+  `;
+}
+// shows the user the correct fault info, basically updating the table that they are shwon
+
+
+
+async function createFault(title, location, severity) {
+  const response = await fetch("/api/faults", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      title: title,
+      location: location,
+      severity: severity
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error("Could not create fault");
+  }
+
+  return response.json();
+}
+// creates a new fault for an item by first taking user input sending it to FastAPI, where the fault is created
+
+
+
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  if (faultPanel) {
+    faultPanel.rotation.y += 0.01;
+  }
+
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", () => {
+  if (!camera || !renderer) return;
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  initThreeScene();
+
+  const scanFaultBtn = document.getElementById("scanFaultBtn");
+  const closeFaultBtn = document.getElementById("closeFaultBtn");
+
+
+  const titleEl = document.getElementById("title");
+  const locationEl = document.getElementById("location");
+  const severityEl = document.getElementById("severity");
+  const addBtn = document.getElementById("addBtn");
+  const msgEl = document.getElementById("msg");
+
+
+
+  scanFaultBtn.addEventListener("click", async () => {
+    current_fault_id = 1;
+
+    try {
+      const fault_data = await getFault(current_fault_id);
+
+      createFaultPanel(fault_data);
+      updateFaultInfo(fault_data);
+
+      closeFaultBtn.disabled = false;
+    } catch (error) {
+      document.getElementById("faultInfo").innerText =
+        "Could not load fault from backend.";
+
+      console.error(error);
+    }
+  });
+
+// complete fault
+  closeFaultBtn.addEventListener("click", async () => {
+    if (!current_fault_id) return;
+
+    try {
+      const updated_fault_data = await updateFault(current_fault_id, "closed");
+
+      createFaultPanel(updated_fault_data);
+      updateFaultInfo(updated_fault_data);
+
+      closeFaultBtn.disabled = true;
+    } catch (error) {
+      document.getElementById("faultInfo").innerText =
+        "Could not update fault.";
+
+      console.error(error);
+    }
+  });
+
+  // create a new fault
+  addBtn.addEventListener("click", async () => {
+    msgEl.textContent = "";
+
+    try {
+      const title = titleEl.value.trim();
+      const location = locationEl.value.trim();
+      const severity = severityEl.value;
+
+      const created_fault = await createFault(title, location, severity);
+
+      msgEl.textContent = `Created fault ID: ${created_fault.id}`;
+
+      titleEl.value = "";
+      locationEl.value = "";
+      severityEl.value = "low";
+
+      current_fault_id = created_fault.id;
+
+      createFaultPanel(created_fault);
+      updateFaultInfo(created_fault);
+
+      closeFaultBtn.disabled = false;
+    } catch (error) {
+      msgEl.textContent = "Error: " + error.message;
+      console.error(error);
+    }
+  });
+});
